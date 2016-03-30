7ce1a29910c4c36da080e436be5f92db1764ca9e
Colin Sullivan <colin.sullivan@apcera.com>
2016-03-30 17:02:13 -0600

    Reduce the amount of time it take a client to time out.
    * Stop gap until STAN can detect a disconnected client
    * Added test.

--- test/test.go
+++ test/test.go
@@ -3,9 +3,23 @@
 package test
 
 import (
+	natsd "github.com/nats-io/gnatsd/server"
 	"github.com/nats-io/stan-server/server"
 )
 
 func RunServer(ID string) *server.StanServer {
 	return server.RunServer(ID)
+}
+
+// Helper to assist debugging
+func RunServerWithDebugTrace(ID string, enableDebug, enableTrace bool) *server.StanServer {
+	opts := &natsd.Options{}
+
+	opts.Debug = enableDebug
+	opts.Trace = enableTrace
+	opts.NoLog = false
+
+	server.EnableDefaultLogger(opts)
+
+	return server.RunServer(ID, opts)
 }
