6bbd7df2edb3a366b2e5678e35fdbc4e526533d9
Ivan Kozlovic <ivan.kozlovic@apcera.com>
2016-04-04 19:10:34 -0600

    Added: tests for util.go

---
+++ util/util_test.go
@@ -0,0 +1,56 @@
+// Copyright 2016 Apcera Inc. All rights reserved.
+
+package util
+
+import (
+	"os"
+	"testing"
+)
+
+func TestEnsureBufBigEnough(t *testing.T) {
+	buf := make([]byte, 3)
+	newBuf := EnsureBufBigEnough(buf, 2)
+	if len(newBuf) != len(buf) {
+		t.Fatal("EnsureBufBigEnough should not have allocated a new buffer")
+	}
+	newBuf = EnsureBufBigEnough(buf, 10)
+	if len(newBuf) <= 10 {
+		t.Fatalf("Buffer should be at least 10, it is: %v", len(newBuf))
+	}
+}
+
+func TestWriteInt(t *testing.T) {
+	fileName := "test.dat"
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	defer os.Remove(fileName)
+	defer file.Close()
+	if err := WriteInt(file, 123); err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+}
+
+func TestReadInt(t *testing.T) {
+	fileName := "test.dat"
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	defer os.Remove(fileName)
+	defer file.Close()
+
+	if _, err := ReadInt(file); err == nil {
+		t.Fatal("Expected an error")
+	}
+	if err := WriteInt(file, 123); err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if _, err := file.Seek(0, 0); err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if v, err := ReadInt(file); err != nil || v != 123 {
+		t.Fatalf("Expected to read 123, got: %v (err=%v)", v, err)
+	}
+}
