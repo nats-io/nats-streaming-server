24c7d7eb8e4518cd4c0c6d8139f0aec607c62dba
Ivan Kozlovic <ivan.kozlovic@apcera.com>
2016-05-05 10:38:12 -0600

    Fix use of default NATS Server options
    Added NoSigs = true to the default NATS Server options. Since the
    NATS Server is embedded, I don't think we need signal handling.
    If we do, we should set NoSigs to true for servers running in test.

--- test/test.go
+++ test/test.go
@@ -3,7 +3,6 @@
 package test
 
 import (
-	natsd "github.com/nats-io/gnatsd/server"
 	"github.com/nats-io/stan-server/server"
 )
 
@@ -15,7 +14,7 @@ func RunServer(ID string) *server.StanServer {
 // RunServerWithDebugTrace is a helper to assist debugging
 func RunServerWithDebugTrace(ID string, enableSTANDebug, enableSTANTrace, enableNATSDebug, enableNATSTrace bool) *server.StanServer {
 
-	nOpts := natsd.Options{}
+	nOpts := server.DefaultNatsServerOptions
 	nOpts.Debug = enableNATSDebug
 	nOpts.Trace = enableNATSTrace
 	nOpts.NoLog = false
