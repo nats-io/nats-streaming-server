a0024fe0158ae098f86bfd0c5a83f5ed94e4ed13
Ivan Kozlovic <ivan.kozlovic@apcera.com>
2016-12-27 10:24:18 -0700

    Bump version for dev [ci skip]

--- server/server.go
+++ server/server.go
@@ -31,7 +31,7 @@ import (
 // Server defaults.
 const (
 	// VERSION is the current version for the NATS Streaming server.
-	VERSION = "0.3.6"
+	VERSION = "0.3.7"
 
 	DefaultClusterID      = "test-cluster"
 	DefaultDiscoverPrefix = "_STAN.discover"
