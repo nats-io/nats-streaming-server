83443201e786ef585bd33777cee3a763c38c97da
Ivan Kozlovic <ivan.kozlovic@apcera.com>
2017-04-26 16:07:05 -0600

    [ADDED] Signal handler for SIGUSR1 for log rotation
    Reworked the signal handling code to include support for the
    log file rotation as done in NATS Server.

---
+++ server/signal_windows.go
@@ -0,0 +1,28 @@
+// Copyright 2017 Apcera Inc. All rights reserved.
+
+package server
+
+import (
+	"os"
+	"os/signal"
+)
+
+// Signal Handling
+func (s *StanServer) handleSignals() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		// We register only 1 signal (os.Interrupt) so we don't
+		// need to check which one we get, since Notify() relays
+		// only the ones that are registered.
+		<-c
+		s.Shutdown()
+		signalMu.RLock()
+		noExit := signalNoExit
+		signalMu.RUnlock()
+		if noExit {
+			return
+		}
+		os.Exit(0)
+	}()
+}
