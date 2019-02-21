d1c2f2e098fd284e19d27504e996d64fd9a274ad
Ivan Kozlovic <ivan@synadia.com>
2019-02-20 19:37:09 -0700

    Bump version for dev [ci skip]
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- server/server.go
+++ server/server.go
@@ -47,7 +47,7 @@ import (
 // Server defaults.
 const (
 	// VERSION is the current version for the NATS Streaming server.
-	VERSION = "0.12.0"
+	VERSION = "0.12.1"
 
 	DefaultClusterID      = "test-cluster"
 	DefaultDiscoverPrefix = "_STAN.discover"
