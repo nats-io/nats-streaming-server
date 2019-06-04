2a67132e38c3fb3ace0bb2b8c0a71d8f08380e67
Ivan Kozlovic <ivan@synadia.com>
2019-06-04 13:17:43 -0600

    Remove RC which prevents go.mod to pull it as the latest
    Go will consider latest the last non pre-release tag. v0.15.0-RC01
    is considered a pre-release in Go, so go mod then pulls v0.14.3.
    
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- server/server.go
+++ server/server.go
@@ -46,7 +46,7 @@ import (
 // Server defaults.
 const (
 	// VERSION is the current version for the NATS Streaming server.
-	VERSION = "0.15.0-RC01"
+	VERSION = "0.15.0"
 
 	DefaultClusterID      = "test-cluster"
 	DefaultDiscoverPrefix = "_STAN.discover"
