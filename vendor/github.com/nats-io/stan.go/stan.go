2ea43e0e3bd28793cc75e7cb8c8d4777c2238724
Ivan Kozlovic <ivan@synadia.com>
2021-01-11 15:58:33 -0700

    [UPDATED] Protobuf dependency
    Updated stan.go dependency to the latest release and regenerated
    the protobuf go file using the latest protobuf v1.3.2 release.
    
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- vendor/github.com/nats-io/stan.go/stan.go
+++ vendor/github.com/nats-io/stan.go/stan.go
@@ -26,7 +26,7 @@ import (
 )
 
 // Version is the NATS Streaming Go Client version
-const Version = "0.8.1"
+const Version = "0.8.2"
 
 const (
 	// DefaultNatsURL is the default URL the client connects to
