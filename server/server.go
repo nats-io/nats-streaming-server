db22f4b41ed0e98828c8bdec9187c6c620568ac1
Ivan Kozlovic <ivan@synadia.com>
2022-03-09 15:23:28 -0700

    Bump version
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- server/server.go
+++ server/server.go
@@ -47,7 +47,7 @@ import (
 // Server defaults.
 const (
 	// VERSION is the current version for the NATS Streaming server.
-	VERSION = "0.24.3"
+	VERSION = "0.24.4-beta"
 
 	DefaultClusterID      = "test-cluster"
 	DefaultDiscoverPrefix = "_STAN.discover"
