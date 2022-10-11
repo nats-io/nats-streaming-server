9e599667e4eb93f964ff87f1c8d6edd3ef7ee5a5
Ivan Kozlovic <ivan@synadia.com>
2022-10-11 14:06:18 -0600

    Bump to v0.25.2
    A change in test framework in between releases has changed the
    SQL setting for tests, which now was enabled by default even
    for server tests. This caused the release process to fail.
    I then pushed the 0.25.1 tag but forgot to first update the
    version in server.go, which was correctly caught by the
    test TestVersionMatchesTag which is used to validate and trigger
    the release.
    
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- server/server.go
+++ server/server.go
@@ -47,7 +47,7 @@ import (
 // Server defaults.
 const (
 	// VERSION is the current version for the NATS Streaming server.
-	VERSION = "0.25.0"
+	VERSION = "0.25.2"
 
 	DefaultClusterID      = "test-cluster"
 	DefaultDiscoverPrefix = "_STAN.discover"
